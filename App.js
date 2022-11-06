172d413ce5dfbb742846f575e023341ba416725f
Junich <junich0911@gmail.com>
2022-11-06 20:17:24 +0900

    不要なコードの削除

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
+// $(function () {})ここでHTMLが全部読み込まれた後に下記の処理をするようにしている
 $(function () {
   // id = 'start-button'を押した時の処理を記述
   $('#start-button').on('click',
     function () {
       if ($('#start-button').text() === "スタート！！") {
-        // console.log("スタートが押されたよ");
         // id = buttonの要素に'disabled', trueを追加する（ここでボタンの制御）
         $('#start-button').prop('disabled', true);
         // id = emojiの要素にε=ヽ(*￣∇￣)ノ文字列を追加している
         $('#emoji').text('ε=ヽ(*￣∇￣)ノ');
         // countに０を代入
         var count = 0;
-        // setTimeoutでfunctionを“5秒後”に実行
+        // setTimeoutでfunctionを“0.5秒後”に実行
         setTimeout(function progress() {
           // countに１づつたす
           count++;
@@ -18,6 +18,7 @@ $(function () {
           if (count <= 10) {
             // ここでid = emojiの要素に文字列ε=を先頭に追加して行っている
             $('#emoji').prepend('ε=');
+            // progress関数を実行する
             setTimeout(progress, 500);
           }
         }, 500);
@@ -29,7 +30,6 @@ $(function () {
       else {
         $('#start-button').text('スタート！！');
         $('#emoji').text('ヽ( ￣∇￣)ノ');
-        // console.log("位置についてが押されたよ");
       }
 
     }
